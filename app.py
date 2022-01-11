a1cb52ca8730300e72ed1e6c92bcb16efba7eadb
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-11 15:16:12 +0100

    ADD do_request for request processing

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import requests, json, yaml, os
 from requests.exceptions import HTTPError
 from flask import Flask
@@ -5,20 +6,22 @@ from flask import render_template
 
 app = Flask(__name__)
 
+def do_request():
+    stream = open(os.getcwd() + "/config.yml", 'r')
+    config = yaml.load(stream, Loader=yaml.FullLoader)
+
+    set_url_api_key = f'https://freecurrencyapi.net/api/v2/latest?apikey={config["api_key"]}&base_currency=TRY'
+
+    req = requests.get(set_url_api_key).json()
+
+    return req
+
 @app.route('/')
 def index():
     try:
-        stream = open(os.getcwd() + "/config.yml", 'r')
-        config = yaml.load(stream, Loader=yaml.FullLoader)
-
-        set_url_api_key = f'https://freecurrencyapi.net/api/v2/latest?apikey={config["api_key"]}&base_currency=TRY'
-
-        req = requests.get(set_url_api_key).text
-        req_loads = json.loads(req)
-
-        get_timestamp = req_loads.get("query").get("timestamp")
-        get_base = req_loads.get("query").get("base_currency")
-        get_data = req_loads.get('data')
+        get_timestamp = do_request().get("query").get("timestamp")
+        get_base = do_request().get("query").get("base_currency")
+        get_data = do_request().get('data')
 
         limiter = 2
         separate_timestamp = ':'.join([str(get_timestamp)[i:i+limiter]
